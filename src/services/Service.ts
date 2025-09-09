c10f208ff91bdf8a5d9e595ff066121f289bc3f6
Vitor Nazareth <vitorzath@gmail.com>
2025-09-08 21:39:12 -0300

    Deploy do Projeto Blog Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://blogpessoal-tz7r.onrender.com', 
+  baseURL: 'baseURL: import.meta.env.VITE_API_URL', 
 });
 
 export const cadastrarUsuario = async (
